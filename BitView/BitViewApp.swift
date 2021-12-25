79714d49a71870dfbbf0124ac96adfb9e0ea7bbb
Abubakir Kais <bakir_janabi@yahoo.com>
2021-12-25 16:43:30 +0400

    Initial Commit

---
+++ BitView/BitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitViewApp.swift
+//  BitView
+//
+//  Created by Abubakir  Kais on 25/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
